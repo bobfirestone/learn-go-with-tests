696c0fc0d6bbedbe20c3ad61de0ffd1ce01701c0
Bob Firestone <bob@neonruby.com>
2019-07-02 15:51:01 -0600

    select before adding concurrency

---
+++ select/racer.go
@@ -0,0 +1,23 @@
+package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+// Racer makes GET requests 2 websites and see which comes back faster
+func Racer(a, b string) (winner string) {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
