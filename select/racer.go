cc7e8bf7d54d2baf09ebaa38712be76164fcb626
Bob Firestone <bob@neonruby.com>
2019-07-02 17:09:34 -0600

    select

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,35 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-// Racer makes GET requests 2 websites and see which comes back faster
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+var tenSecondTimeout = 10 * time.Second
 
-	if aDuration < bDuration {
-		return a
-	}
-	return b
+// Racer makes GET requests 2 websites and see which comes back faster. Returns an error if neither site has responded in 10 seconds per requirement.
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+// ConfigurableRacer is the race logic & gives the ability to change the tiemout
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
