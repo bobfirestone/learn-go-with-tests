1fa9b77ac5ae4695a716e3bf6c9c99af2da740ed
Bob Firestone <bob@neonruby.com>
2019-07-02 17:35:56 -0600

    reflection just strings so far

---
+++ reflection/walk_test.go
@@ -0,0 +1,43 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "Atlanta"},
+			[]string{"Chris", "Atlanta"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("Got %v want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
